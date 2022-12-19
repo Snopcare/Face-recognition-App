a26fb51478afa5a4082b8d9396ac993b666cdcf7
Błażej <116065750+Snopcare@users.noreply.github.com>
2022-12-19 21:45:52 +0100

    Adding components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return(
     <div className="App">
-      {/*<Navigation />
-        <Logo />
-          <ImageLinkForm />
-          <FaceRecognition />*/}
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+            {/*<FaceRecognition />*/}
     </div>
   );
 };
