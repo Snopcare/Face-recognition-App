f338d4949fcc8917bbadf92f9f835b19af84da4c
Błażej <116065750+Snopcare@users.noreply.github.com>
2023-01-14 15:25:13 +0100

    Bcrypt hash - register

--- Backend Api/server.js
+++ Backend Api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 const app = express();
 
@@ -41,6 +42,9 @@ app.post('/signin', (req,res) => {
 
 app.post('/register',(req,res) => {
 	const {email, name, password} = req.body;
+	bcrypt.hash(password, null, null, function(err, hash) {
+		console.log(hash);
+});
 	database.users.push({
 		id:'125',
 		name: name,
@@ -81,17 +85,13 @@ const { id } = req.body;
 	}
 })
 
-bcrypt.hash("bacon", null, null, function(err, hash) {
-  // Store hash in your password DB.
-});
-
 // Load hash from your password DB.
-bcrypt.compare("bacon", hash, function(err, res) {
-    // res == true
-});
-bcrypt.compare("veggies", hash, function(err, res) {
-    // res = false
-});
+// bcrypt.compare("bacon", hash, function(err, res) {
+//     // res == true
+// });
+// bcrypt.compare("veggies", hash, function(err, res) {
+//     // res = false
+// });
 
 
 
