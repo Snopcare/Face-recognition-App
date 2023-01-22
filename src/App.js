5cc1d2bb22232cea972a9dc843baf5388e277c72
Błażej <116065750+Snopcare@users.noreply.github.com>
2023-01-22 13:44:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ class App extends Component {
       }
   }
 
+  componnentDidMount() {
+    fetch('localhost:3003')
+    .then(response => response.json())
+    .then(console.log)
+  }
+
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
 
-const Signin = ({onRouteChange}) => {
+class Signin extends React.Component {
+constructor(props) {
+	super(props);
+	this.state = {
+		signInEmail: '',
+		signInPassword: ''
+	}
+}
+onEmailChange = (event) => {
+	this.setState({signInEmail: event.target.value})
+}
+
+onPasswordChange = (event) => {
+	this.setState({signInPassword: event.target.value})
+}
+
+onSubmitSignIn = () => {
+	console.log(this.state);
+}
+
+render() {
 	return (
 	 <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 		<main className="pa4 black-80">
@@ -19,19 +39,20 @@ const Signin = ({onRouteChange}) => {
 		    </fieldset>
 		    <div className="">
 		      <input
-		      onClick={() => onRouteChange('home')}
+		      onClick={() => this.props.onRouteChange('home')}
 		      className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 		      type="submit" 
 		      value="Sign in"
 		      />
 		    </div>
 		    <div className="lh-copy mt3">
-		      <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+		      <p onClick={() => this.props.onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
 		    </div>
 		  </form>
 		</main>
 	</article>
 			);
+}
 }
 
 export default Signin;
