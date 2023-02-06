7d7c7cce25858d874e8e5fc8bb31b7a5f507a2f0
Błażej <116065750+Snopcare@users.noreply.github.com>
2023-02-06 21:53:32 +0100

    Prep for Deployment

--- Backend Api/controllers/signin.js
+++ Backend Api/controllers/signin.js
@@ -1,8 +1,8 @@
 const handleSignin = (req,res, db, bcrypt) => {
 	const {email, password } = req.body;
-	if (!email || !password) {
-		return res.status(400).json('incorrect form submission');
-	}
+	// if (!email || !password) {
+	// 	return res.status(400).json('incorrect form submission');
+	// }
 	db.select('email', 'hash').from('login')
 	.where('email','=', email)
 	.then(data => {
